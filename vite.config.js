62477c56b3454a186495508db6d6bbe6c6a3ae2f
danielzuev <up2016078@myport.ac.uk>
2024-04-24 09:36:16 +0100

    trying to udpate the routing of the app

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist', // Ensure this points to where you want your build files to go
+    rollupOptions: {
+      input: 'public/index.html'
+    }
+  }
 })
