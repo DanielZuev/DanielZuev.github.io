146a177196abc282a21c5f360b4a59f14654f584
danielzuev <up2016078@myport.ac.uk>
2024-04-24 12:56:24 +0100

    reverting back to broswerouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-// import { HashRouter, Routes, Route } from 'react-router-dom';
-// import { HashRouter, Route, Routes } from "react-router-dom"
-
 import Login from "./auth/Login"
 import Register from "./auth/Register"
 import { AuthProvider } from "./context/AuthContext"
@@ -14,6 +11,7 @@ import FindRoom from "./Layout/FindRoom"
 import MySessions from "./Layout/Sessions/MySessions"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/style.css'
+
 function App() {
 
   return (
@@ -24,13 +22,12 @@ function App() {
                   <Route path="/register" element={<Register />} />
                   
                   {/* Add more protected routes as needed */}
-                  <Route path="/" element={<ProtectedRoute element={<Home />} />} />
-                  <Route path="/userroom" element={<ProtectedRoute element={<CreateUserRoom />} />} />
-                  <Route path="/myrooms" element={<ProtectedRoute element={<MyRooms />} />} />
-                  <Route path="/mysessions" element={<ProtectedRoute element={<MySessions />} />} />
-                  <Route path="/roomByName/:roomName" element={<ProtectedRoute element={<Room />} />} />
-                  <Route path="/findRoom" element={<ProtectedRoute element={<FindRoom />} />} />
-                  {/* <Route path="*" element={<ProtectedRoute component={Home} />} /> */}
+                  <Route path="/" element={<ProtectedRoute component={Home} />} />
+                  <Route path="/userroom" element={<ProtectedRoute component={CreateUserRoom} />} />
+                  <Route path="/myrooms" element={<ProtectedRoute component={MyRooms} />} />
+                  <Route path="/mysessions" element={<ProtectedRoute component={MySessions} />} />
+                  <Route path="/roomByName/:roomName" element={<ProtectedRoute component={Room} />} />
+                  <Route path="/findRoom" element={<ProtectedRoute component={FindRoom} />} />
                   
                 </Routes>
             </Router>
--- src/Routing/ProtectedRoute.jsx
+++ src/Routing/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ProtectedRoute = ({ element, ...rest }) => {
+const ProtectedRoute = ({ component: Component, ...rest }) => {
   const isAuthenticated = localStorage.getItem('token');
 
   // If not authenticated, redirect to the login page
@@ -9,47 +9,13 @@ const ProtectedRoute = ({ element, ...rest }) => {
     return <Navigate to="/login" replace />;
   }
 
-  // If authenticated, render the element passed to ProtectedRoute
-  return React.cloneElement(element, { ...rest });
+  // If authenticated, render the component passed to ProtectedRoute
+  return <Component {...rest} />;
 };
 
 // PropTypes
 ProtectedRoute.propTypes = {
-  element: PropTypes.element.isRequired,
+  component: PropTypes.elementType.isRequired,
 };
 
 export default ProtectedRoute;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Navigate } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-
-// const ProtectedRoute = ({ component: Component, ...rest }) => {
-//   const isAuthenticated = localStorage.getItem('token');
-
-//   // If not authenticated, redirect to the login page
-//   if (!isAuthenticated) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   // If authenticated, render the component passed to ProtectedRoute
-//   return <Component {...rest} />;
-// };
-
-// // PropTypes
-// ProtectedRoute.propTypes = {
-//   component: PropTypes.elementType.isRequired,
-// };
-
-// export default ProtectedRoute;
