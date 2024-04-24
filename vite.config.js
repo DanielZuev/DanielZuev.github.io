f2a8a25d5c0220e2ccac431f0a16853ee3507b9e
danielzuev <up2016078@myport.ac.uk>
2024-04-24 10:35:49 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist', // Ensure this points to where you want your build files to go
-    rollupOptions: {
-      input: 'public/index.html'
-    }
-  }
 })
